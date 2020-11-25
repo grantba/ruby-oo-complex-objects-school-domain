d3b2d6adf9e094dab3c2d95e44564d2da311d4ec
Blaire <grant.blaire@gmail.com>
2020-11-24 21:53:16 -0600

    commit tree f149a3d79baaf9395bdc5f993d56fcf0908b9faf

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if roster[grade]
+        roster[grade] << name
+        else
+            roster[grade] = []
+            roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        return roster[grade]
+    end
+
+    def sort
+        hash_new = {}
+        roster.each do |grade, name|
+            hash_new[grade] = name.sort
+        end
+        hash_new
+    end
+
+end
+
